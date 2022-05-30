a3ec1e5e33239c83e5a780b7bbb1428543676cbe
Jung, Jaewan <wodhkszxc@gmail.com>
2022-05-31 01:53:22 +0900

    lab1_2, hw1_2와 동일. 정수형 예약 번호, 시작시간, 종료시간으로 구성된 예약을 표현하는 클래스

---
+++ 알고리즘/src/hw4_1/Reservation.java
@@ -0,0 +1,41 @@
+package hw4_1;
+//예약 번호, 시작시간, 종료시간 정보를 갖는 예약을 표현하는 클래스
+public class Reservation {
+		private int id;
+		private int startTime;
+		private int endTime;
+		// 예약 번호, 시작시간, 종료시간을 매개변수로 받아 초기화 하는 생성자
+		public Reservation(int id, int startTime, int endTime) {
+			this.id = id;
+			this.startTime = startTime;
+			this.endTime = endTime;
+		}
+		public int getId() {
+			return id;
+		}
+		
+		public int getStartTime() {
+			return startTime;
+		}
+		
+		public void setStartTime(int startTime) {
+			this.startTime = startTime;
+		}
+
+		public int getEndTime() {
+			return endTime;
+		}
+
+		public void setEndTime(int endTime) {
+			this.endTime = endTime;
+		}
+		// 예약 시간 길이를 계산하여 리턴
+		public int getLength() {
+			return endTime - startTime;
+		}
+		
+		@Override
+		public String toString() {
+			return id + " " + startTime + "시~" + endTime + "시 " + getLength() + "시간";
+		}
+}
